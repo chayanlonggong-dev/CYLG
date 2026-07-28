ef59b2bf6134ac7c3c07e3cefb9621e1f6dbbb85
chayanlonggong-dev <chayanlonggong@gmail.com>
2026-07-29 01:42:57 +0700

    test

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 
+
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+
 export const prisma =
   global.prisma ??
   new PrismaClient({
-    log: ["error", "warn"],
+    log: [
+      "error",
+      "warn",
+    ],
   });
 
+
+
 if (process.env.NODE_ENV !== "production") {
+
   global.prisma = prisma;
+
 }
